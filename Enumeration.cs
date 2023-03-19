6c841bec006b0355f5c6ff21973f4360909e45f3
Abrar Ahmed <102857826+abrar2030@users.noreply.github.com>
2023-03-19 20:02:33 +0100

    Update Enumeration.cs

--- Enumeration.cs
+++ Enumeration.cs
@@ -1,97 +1,95 @@
-    using System;
-    using System.Collections;
+        using System;
+        using System.Collections;
 
-    class Program
-    {
-        static void Main(string[] args)
+        class Program
         {
-            
-        int[] numbers = { 1, 2, 3, 4, 5 };
-    IEnumerator enumerator = numbers.GetEnumerator();
+            static void Main(string[] args)
+            {
 
-    while (enumerator.MoveNext())
-    {
-        Console.WriteLine(enumerator.Current);
-    }
+            int[] numbers = { 1, 2, 3, 4, 5 };
+        IEnumerator enumerator = numbers.GetEnumerator();
 
-    List<string> fruits = new List<string> { "apple", "banana", "cherry" };
-    IEnumerator<string> enumerator = fruits.GetEnumerator();
-
-    while (enumerator.MoveNext())
-    {
-        Console.WriteLine(enumerator.Current);
-    }
-
-    LinkedList<int> list = new LinkedList<int>();
-    list.AddLast(1);
-    list.AddLast(2);
-    list.AddLast(3);
-
-    IEnumerator<int> enumerator = list.GetEnumerator();
-    while (enumerator.MoveNext())
-    {
-       Console.WriteLine(enumerator.Current);
-    }
-
-    Queue<int> queue = new Queue<int>();
-    queue.Enqueue(1);
-    queue.Enqueue(2);
-    queue.Enqueue(3);
-
-    IEnumerator enumerator = queue.GetEnumerator();
-    while (enumerator.MoveNext())
-    {
-      Console.WriteLine(enumerator.Current);
-    }
-
-    Stack<int> stack = new Stack<int>();
-    stack.Push(1);
-    stack.Push(2);
-    stack.Push(3);
-
-    IEnumerator enumerator = stack.GetEnumerator();
-    while (enumerator.MoveNext())
-    {
-       Console.WriteLine(enumerator.Current);
-    }
-
-    Dictionary<string, int> ages = new Dictionary<string, int>
-    {
-        { "Alice", 25 },
-        { "Bob", 30 },
-        { "Charlie", 35 }
-    };
-
-    IEnumerator<KeyValuePair<string, int>> enumerator = ages.GetEnumerator();
-    while (enumerator.MoveNext())
-    {
-        KeyValuePair<string, int> entry = enumerator.Current;
-        string name = entry.Key;
-        int age = entry.Value;
-        Console.WriteLine($"{name} is {age} years old");
-    }
-
-
-        Hashtable hashtable = new Hashtable();
-        hashtable.Add("apple", 5);
-        hashtable.Add("banana", 2);
-        hashtable.Add("orange", 3);
-        
-        IEnumerator enumerator = hashtable.GetEnumerator();
         while (enumerator.MoveNext())
         {
-            HashtableEntry entry = (HashtableEntry)enumerator.Current;
-            Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+            Console.WriteLine(enumerator.Current);
         }
-    
 
-    HashSet<string> set = new HashSet<string> { "apple", "banana", "cherry" };
-    IEnumerator<string> enumerator = set.GetEnumerator();
+        List<string> fruits = new List<string> { "apple", "banana", "cherry" };
+        IEnumerator<string> enumerator = fruits.GetEnumerator();
 
-    while (enumerator.MoveNext())
-    {
-       Console.WriteLine(enumerator.Current);
-    }
-
+        while (enumerator.MoveNext())
+        {
+            Console.WriteLine(enumerator.Current);
         }
-    }
+
+        LinkedList<int> list = new LinkedList<int>();
+        list.AddLast(1);
+        list.AddLast(2);
+        list.AddLast(3);
+
+        IEnumerator<int> enumerator = list.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+           Console.WriteLine(enumerator.Current);
+        }
+
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+
+        IEnumerator enumerator = queue.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+          Console.WriteLine(enumerator.Current);
+        }
+
+        Stack<int> stack = new Stack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        IEnumerator enumerator = stack.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+           Console.WriteLine(enumerator.Current);
+        }
+
+        Dictionary<string, int> ages = new Dictionary<string, int>
+        {
+            { "Alice", 25 },
+            { "Bob", 30 },
+            { "Charlie", 35 }
+        };
+
+        IEnumerator<KeyValuePair<string, int>> enumerator = ages.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            KeyValuePair<string, int> entry = enumerator.Current;
+            string name = entry.Key;
+            int age = entry.Value;
+            Console.WriteLine($"{name} is {age} years old");
+        }
+
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("apple", 5);
+            hashtable.Add("banana", 2);
+            hashtable.Add("orange", 3);
+
+            IEnumerator enumerator = hashtable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                HashtableEntry entry = (HashtableEntry)enumerator.Current;
+                Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+            }
+
+        HashSet<string> set = new HashSet<string> { "apple", "banana", "cherry" };
+        IEnumerator<string> enumerator = set.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+           Console.WriteLine(enumerator.Current);
+        }
+
+            }
+        }
