a1515a1b90601ed8f59fa2cfb90db1c07e17c173
Abrar Ahmed <102857826+abrar2030@users.noreply.github.com>
2023-03-19 23:52:35 +0100

    Create Collections.cpp

---
+++ Collections.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int arr[5] = {1, 2, 3, 4, 5};  // initializing an array of integers with 5 elements
+cout << arr[0];                // accessing the first element of the array (output: 1)
+
+vector<int> v = {1, 2, 3, 4, 5};  // initializing a vector of integers with 5 elements
+v.push_back(6);                   // adding a new element to the end of the vector
+cout << v[5];                     // accessing the last element of the vector (output: 6)
+
+list<int> l = {1, 2, 3, 4, 5};   // initializing a list of integers with 5 elements
+l.push_back(6);                  // adding a new element to the end of the list
+l.push_front(0);                 // adding a new element to the front of the list
+cout << l.front() << " " << l.back();  // accessing the first and last elements of the list (output: 0 6)
+
+set<int> s = {3, 1, 4, 1, 5, 9, 2};  // initializing a set of integers with 5 elements
+s.insert(6);                        // adding a new element to the set
+s.erase(4);                         // removing an element from the set
+cout << s.count(9);                 // checking if an element exists in the set (output: 1)
+
+map<string, int> m;                // initializing an empty map with string keys and integer values
+m["apple"] = 5;                    // adding a new key-value pair to the map
+m["banana"] = 2;
+m["cherry"] = 7;
+cout << m["banana"];               // accessing the value of a key in the map (output: 2)
+
+	return 0;
+}
